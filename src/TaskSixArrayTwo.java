0326616ebb417c2016900a5bf99ef329d2b40215
Вячеслав Блинков <xristian860810@gmail.com>
2022-06-24 22:57:06 +0300

    Выполниз домашнее задание 5 , массив , часть2, правки внесены повторно. Задача 2 сделана через цикл

--- src/TaskSixArrayTwo.java
+++ src/TaskSixArrayTwo.java
@@ -1,32 +1,34 @@
-import java.util.Arrays;
 public class TaskSixArrayTwo {
     public static void main(String[] args) {
-        java.util.Random random = new java.util.Random();
         // Задача 1
         System.out.println("Задача 1");
         int[] numDays = new int[30];
         float sum = 0;
-        float average = 0f;
+        float average;
         for (int i = 0; i < numDays.length; i++) {
-            numDays[i] = random.nextInt(100_000) + 100_000;
+            numDays[i] = RandomNumber.generateRandomArray()[i];
             System.out.println("Сумма расходов за " + (i + 1) + " день " + numDays[i] + " рублей");
         }
         for (int i = 0; i < numDays.length; i++) {
             sum += numDays[i];
         }
-        System.out.println("Сумма расходов за месяц равна: " + "" + sum + " рублей");
+        System.out.println("Сумма расходов за месяц равна: " + sum + " рублей");
         System.out.println();
         System.out.println("Задача 2 ");
         // Задача 2
-        Arrays.sort(numDays);
-        int min = 0;
-        for (int i = 0; i < numDays.length; i++) {
-            if (numDays[i] < numDays[i++]) {
+        int min = numDays[0];
+        for (int i = 1; i < numDays.length; i++) {
+            if (min > numDays[i]) {
                 min = numDays[i];
             }
         }
-        System.out.println("минимальное значение:" + numDays[min]);
-        System.out.println("максимальное значение:" + numDays[numDays.length - 1]);
+        int max = numDays[0];
+        for (int i = 1; i < numDays.length; i++) {
+            if (max < numDays[i])
+                max = numDays[i];
+        }
+        System.out.println("минимальное значение:" + min);
+        System.out.println("максимальное значение:" + max);
         System.out.println();
         System.out.println("Задание 3 ");
         average = sum / numDays.length;
